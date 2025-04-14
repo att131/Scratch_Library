7ca16409e2e50f6596ce1a4bc86e6812657c26ce
att131 <58440549+att131@users.noreply.github.com>
2025-04-13 17:22:37 -0700

    Update invite_to_studio_manual.py

--- Scratch_Library_main/invite_to_studio_manual.py
+++ Scratch_Library_main/invite_to_studio_manual.py
@@ -1,4 +1,4 @@
-from Scratch_Library_Main.Scratch_Library_Main._scratch_website_lib import Studio, MANUAL_PATH
+from Scratch_Library_main.Scratch_Library_main._scratch_website_lib import Studio, MANUAL_PATH
 
 # FUNCTIONS
 
